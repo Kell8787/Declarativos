09fdb40676ca3dd80041f742018a0d6d7cc4c4ab
David <105942125+dmejiaoliva-devs@users.noreply.github.com>
2025-08-25 16:41:47 -0600

    Agreagando Historia de Aldemir

---
+++ Actividades/Historia Aldemir.pl
@@ -0,0 +1,64 @@
+%Familia Rivoria
+esposo(esteban, mariela).
+
+padre(esteban, clara).
+padre(esteban, rodolfo).
+padre(esteban, ismael).
+
+madre(mariela, clara).
+madre(mariela, rodolfo).
+madre(mariela, ismael).
+
+%Clara y Andrés
+esposa(clara, andres).
+
+padre(andres, sofia).
+madre(clara, sofia).
+
+%Rodolfo y Teresa
+esposo(rodolfo, teresa).
+
+padre(rodolfo, lucas).
+padre(rodolfo, martina).
+
+madre(teresa, lucas).
+madre(teresa, martina).
+
+%Ismail
+gay(ismael).
+gay(hector).
+borracho(ismael).
+amigo(ismael, hector).
+
+%Familia Montelar
+esposo(alonso, beatriz).
+
+padre(alonso, felipe).
+padre(alonso, catalina).
+padre(alonso, emilia).
+
+madre(beatriz, felipe).
+madre(beatriz, catalina).
+madre(beatriz, emilia).
+
+%Catalina y Andrés
+esposa(catalina, andres).
+
+padre(andres, sofia).
+madrastra(catalina, sofia).
+
+%Oficiio
+herrero(hector).
+tejedora(teresa).
+maestra(clara).
+curandera(catalina).
+explorador(felipe).
+ceramista(emilia).
+agricultor(mateo).
+ganadero(irene).
+aprendiz(lucas, hector).
+
+%informacion npc
+esposo(mateo, irene).
+
+
